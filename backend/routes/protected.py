a422f9584569351e32a243d1f8beefc5abcd1e5f
vidal-Rosc <vidal.rocca86@gmail.com>
2024-09-19 18:06:43 +0200

    .env creation, route.protected modified

--- backend/routes/protected.py
+++ backend/routes/protected.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter, Depends
 from core.security import get_current_user
 
-# Crear un router para las rutas protegidas
 protected_router = APIRouter()
 
-# Ruta protegida, solo accesible si el token JWT es válido
+# Ruta protegida accesible solo con un token JWT válido
 @protected_router.get("/protected")
 async def read_protected_route(current_user: str = Depends(get_current_user)):
-    return {"message": f"Welcome, {current_user}. Access granted to this protected route."}
+    return {"message": f"Bienvenido, {current_user}. Access granted to this protected route."}
