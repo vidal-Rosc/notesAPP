ebfeb3ad4dfde2ce79188a7ce745c8d060bc087f
vidal-Rosc <vidal.rocca86@gmail.com>
2024-09-19 18:03:47 +0200

    routes.auth modified

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -15,21 +15,22 @@ users_collection = db["users"]
 async def register(username: str, password: str):
     user = users_collection.find_one({"username": username})
     if user:
-        raise HTTPException(status_code=400, detail="El usuario ya existe")
+        raise HTTPException(status_code=400, detail="User already registered")
     
     hashed_password = get_password_hash(password)
     users_collection.insert_one({"username": username, "password": hashed_password})
     
-    return {"message": "User Susccesfully registered"}
+    return {"message": "User susccesfullz registered"}
 
 # Ruta para hacer login
 @auth_router.post("/login")
 async def login(username: str, password: str):
     user = users_collection.find_one({"username": username})
     if not user or not verify_password(password, user["password"]):
-        raise HTTPException(status_code=400, detail="Wrong credentials")
+        raise HTTPException(status_code=400, detail="Invalid credentials")
     
     # Crear token JWT
     access_token = create_access_token(data={"sub": username})
     return {"access_token": access_token, "token_type": "bearer"}
+
 
