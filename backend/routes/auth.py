1ed33092533125b7ee3ab1f3904b586b5f67f807
vidal-Rosc <vidal.rocca86@gmail.com>
2024-09-19 22:04:57 +0200

    routes auth modified

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, Depends
 from pymongo import MongoClient
 from core.security import create_access_token, get_password_hash, verify_password
 from core.config import MONGODB_URI, MONGODB_DB
+from core.schemas import UserCreate, UserLogin  # Importamos los esquemas
 
 auth_router = APIRouter()
 
@@ -12,25 +13,26 @@ users_collection = db["users"]
 
 # Ruta para registrar un nuevo usuario
 @auth_router.post("/register")
-async def register(username: str, password: str):
-    user = users_collection.find_one({"username": username})
-    if user:
+async def register(user: UserCreate):
+    existing_user = users_collection.find_one({"username": user.username})
+    if existing_user:
         raise HTTPException(status_code=400, detail="User already registered")
     
-    hashed_password = get_password_hash(password)
-    users_collection.insert_one({"username": username, "password": hashed_password})
+    hashed_password = get_password_hash(user.password)
+    users_collection.insert_one({"username": user.username, "password": hashed_password})
     
-    return {"message": "User susccesfullz registered"}
+    return {"message": "User susccesfully registered"}
 
 # Ruta para hacer login
 @auth_router.post("/login")
-async def login(username: str, password: str):
-    user = users_collection.find_one({"username": username})
-    if not user or not verify_password(password, user["password"]):
-        raise HTTPException(status_code=400, detail="Invalid credentials")
+async def login(user: UserLogin):
+    existing_user = users_collection.find_one({"username": user.username})
+    if not existing_user or not verify_password(user.password, existing_user["password"]):
+        raise HTTPException(status_code=400, detail="Invalid Credentials")
     
     # Crear token JWT
-    access_token = create_access_token(data={"sub": username})
+    access_token = create_access_token(data={"sub": user.username})
     return {"access_token": access_token, "token_type": "bearer"}
+
 
 
