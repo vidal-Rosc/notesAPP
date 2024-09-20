61b996d85536c76f8df2997e0ed92912a738740b
vidal-Rosc <vidal.rocca86@gmail.com>
2024-09-20 21:29:31 +0200

    Updating the refresh token duration

--- backend/core/config.py
+++ backend/core/config.py
@@ -8,8 +8,8 @@ load_dotenv()
 # Configuración de JWT y base de datos
 SECRET_KEY = os.getenv("SECRET_KEY", "your_default_secret_key")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = 30  # Duración del token en minutos
-REFRESH_TOKEN_EXPIRE_MINUTES = 10080
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+REFRESH_TOKEN_EXPIRE_MINUTES = int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES", 60*24*7))  # 7 días
 
 # Configuración de MongoDB
 MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://admin:Lamasrobada2803@cluster0.ypalzi1.mongodb.net/")
