3f01fe9b5ccd71f4e83ed58a9fa47b821f954dd1
vidal-Rosc <vidal.rocca86@gmail.com>
2024-09-20 17:45:03 +0200

    modifying routes.auth

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter, HTTPException, Depends
 from pymongo import MongoClient
-from core.security import create_access_token, get_password_hash, verify_password, get_current_user
+from core.security import create_access_token, verify_password, get_current_user
 from core.config import MONGODB_URI, MONGODB_DB
 from core.schemas import UserCreate, UserLogin  # Importamos los esquemas
+from models.user import create_user, get_user_by_username
 
-auth_router = APIRouter()
+auth_router = APIRouter(
+    prefix="/auth",
+    tags=["Autenticación"]
+)
 
 # Conectar a MongoDB
 client = MongoClient(MONGODB_URI)
@@ -12,21 +16,19 @@ db = client[MONGODB_DB]
 users_collection = db["users"]
 
 # Ruta para registrar un nuevo usuario
-@auth_router.post("/register")
+@auth_router.post("/register", response_model=dict)
 async def register(user: UserCreate):
-    existing_user = users_collection.find_one({"username": user.username})
+    existing_user = get_user_by_username(db, user.username)
     if existing_user:
         raise HTTPException(status_code=400, detail="User already registered")
     
-    hashed_password = get_password_hash(user.password)
-    users_collection.insert_one({"username": user.username, "password": hashed_password})
-    
-    return {"message": "User susccesfully registered"}
+    user_id = create_user(db, user)
+    return {"message": "User susccesfully registered", "user_id": user_id}
 
 # Ruta para hacer login
-@auth_router.post("/login")
+@auth_router.post("/login", response_model=dict)
 async def login(user: UserLogin):
-    existing_user = users_collection.find_one({"username": user.username})
+    existing_user = get_user_by_username(db, user.username)
     if not existing_user or not verify_password(user.password, existing_user["password"]):
         raise HTTPException(status_code=400, detail="Invalid Credentials")
     
--- backend/routes/protected.py
+++ backend/routes/protected.py
@@ -3,7 +3,7 @@ from core.security import get_current_user
 
 protected_router = APIRouter()
 
-# Ruta protegida accesible solo con un token JWT válido
+# Ruta protegida para pruebas accesible solo con un token JWT válido
 @protected_router.get("/protected")
-async def read_protected_route(current_user: str = Depends(get_current_user)):
-    return {"message": f"Bienvenido, {current_user}. Access granted to this protected route."}
+async def protected_route(current_user: str = Depends(get_current_user)):
+    return {"message": f"Welcome, {current_user}. Access granted to this protected route."}
