115ba895d26b4e155efe5e57f6ef3c6bd72a01d4
vidal-Rosc <vidal.rocca86@gmail.com>
2024-09-20 21:35:30 +0200

    Updating config.py fixing a huge error :)

--- backend/core/config.py
+++ backend/core/config.py
@@ -6,12 +6,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Configuración de JWT y base de datos
-SECRET_KEY = os.getenv("SECRET_KEY", "your_default_secret_key")
+SECRET_KEY = os.getenv("SECRET_KEY", "almost_got_me")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 REFRESH_TOKEN_EXPIRE_MINUTES = int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES", 60*24*7))  # 7 días
 
 # Configuración de MongoDB
-MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://admin:Lamasrobada2803@cluster0.ypalzi1.mongodb.net/")
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
 MONGODB_DB = os.getenv("MONGODB_DB", "notas_db")
 
