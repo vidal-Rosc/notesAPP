09e1c91ff1a8538c770a0956c0aae0f583ade116
vidal-Rosc <vidal.rocca86@gmail.com>
2024-09-20 21:09:46 +0200

    Updating the user model to manage the access tokens and refresh tokens

--- backend/models/user.py
+++ backend/models/user.py
@@ -11,7 +11,8 @@ def create_user(db, user: UserCreate):
     hashed_password = get_password_hash(user.password)
     user_dict = {
         "username": user.username,
-        "password": hashed_password
+        "password": hashed_password,
+        "refresh_tokens": []  # Lista para almacenar los refresh tokens
     }
     result = users.insert_one(user_dict)
     return str(result.inserted_id)
@@ -20,3 +21,27 @@ def get_user_by_username(db, username: str):
     users = get_users_collection(db)
     return users.find_one({"username": username})
 
+##### Tokens #####
+# Agrega un nuevo refresh token al usuario.
+def add_refresh_token(db, username: str, refresh_token: str):
+    users = get_users_collection(db)
+    users.update_one(
+        {"username": username},
+        {"$push": {"refresh_tokens": refresh_token}}
+    )
+
+#  Elimina un refresh token específico del usuario
+def remove_refresh_token(db, username: str, refresh_token: str):
+    users = get_users_collection(db)
+    users.update_one(
+        {"username": username},
+        {"$pull": {"refresh_tokens": refresh_token}}
+    )
+
+#  Elimina todos los refresh tokens del usuario 
+def remove_all_refresh_tokens(db, username: str):
+    users = get_users_collection(db)
+    users.update_one(
+        {"username": username},
+        {"$set": {"refresh_tokens": []}}
+    )
