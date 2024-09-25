f5177c6e49bc2bc3e44b9f9f83fa1326883bc6f3
vidal-Rosc <vidal.rocca86@gmail.com>
2024-09-25 14:01:02 +0200

    JWT

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import GlobalStyles from './styles/GlobalStyles';
 
 const App = () => {
+  const isAuthenticated = () => {
+    const token = localStorage.getItem('access_token');
+    if (!token) return false;
+
+    try {
+      const decodedToken = jwtDecode(token);
+      const currentTime = Date.now() / 1000; // tiempo en segundos
+      // Verifica si el token ha expirado
+      if (decodedToken.exp < currentTime) {
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('refresh_token');
+        return false;
+      }
+      return true;
+    } catch (error) {
+      // Si no se puede decodificar el token, eliminarlo y retornar false
+      localStorage.removeItem('access_token');
+      localStorage.removeItem('refresh_token');
+      return false;
+    }
+  };
 
   return (
     <>
       <GlobalStyles />
       <Router>
         <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+          <Route
+            path="/"
+            element={
+              isAuthenticated() ? <Navigate to="/dashboard" /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/login"
+            element={isAuthenticated() ? <Navigate to="/dashboard" /> : <LoginPage />}
+          />
+          <Route
+            path="/dashboard"
+            element={isAuthenticated() ? <DashboardPage /> : <Navigate to="/login" />}
+          />
           {/* Agregar más rutas aquí en el futuro */}
         </Routes>
       </Router>
@@ -21,3 +55,4 @@ const App = () => {
 };
 
 export default App;
+
